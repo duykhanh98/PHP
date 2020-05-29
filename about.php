300069aa6aa334f676baecff81e2b0cb9a25ca96
duykhanh98 <50897170+duykhanh98@users.noreply.github.com>
2020-05-29 22:16:45 +0700

    sua file about.php

--- about.php
+++ about.php
@@ -1,2 +1,2 @@
 <p>Mọi chi tiết xin liên hệ</p>
-<label>Không còn thời gian, xin hãy cùng nhau làm! Không thì bỏ thôi, chứ hết cách</label>
+<label>Không còn thời gian, xin hãy cùng nhau làm! Không thì bỏ thôi, thôi toang rồi!</label>
