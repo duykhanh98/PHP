32e070908bc8767a0b4cca69f30d5f0dccc37394
duykhanh98 <50897170+duykhanh98@users.noreply.github.com>
2020-05-30 09:02:57 +0700

    Modi file index.php 30_05

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>CCPTPM</title>
+	<title>BÁO CÁO ĐỒ ÁN MÔN HỌC</title>
 </head>
 <body>
 	<label>Hỏi mà ai cũng im thế nhỉ?</label>
